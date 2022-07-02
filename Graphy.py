04887f1999584fbbc4e61e3f8751708d14d3bbb7
Alonso-20 <alonso_ca_30@hotmail.com>
2022-07-02 10:59:58 -0500

    Añadir caminos #14

--- Graphy.py
+++ Graphy.py
@@ -1,5 +1,6 @@
 import csv
 import Tools
+from perlin_noise import PerlinNoise
 
 class Grapho:
     def __init__(self):
@@ -10,6 +11,11 @@ class Grapho:
         shortRoute = []
         firstAlternativeRoute = []
         secondAlternativeRoute = []
+        #self.shortRoute = []
+        #self.firstAlternativeRoute = []
+        #self.secondAlternativeRoute = []
+        self.pic = []
+
     def create(self,path = 'Data_+1000.csv' ):
         with open(path) as archivo:
             add = set()
@@ -25,8 +31,13 @@ class Grapho:
                         self.nodos.append([c1,c2,float(x),float(y)])
                         self.pnodos.append([float(x),float(y)])
     
-    
+    def createTraffic(self, time):
+        noise = PerlinNoise(octaves=5*(time/100), seed=1981)
+        xpix, ypix = len(self.nodos), len(self.nodos)
+        self.pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
+
     def createAdjList(self):
+        #self.createTraffic(19)
         cont = 0
         flag0 = True
         flag1 = True
@@ -34,49 +45,39 @@ class Grapho:
             self.adjList.append([])
         for i in range(len(self.nodos) - 1):
             for j in range(i , len(self.nodos) - 1):
-                #while(cont < 2):
                 if flag0 and self.nodos[i][0] == (self.nodos[j + 1][0] or self.nodos[j + 1][1]):
                     d = self.t.distance(self.nodos[i][2], self.nodos[i][3],self.nodos[j + 1][2], self.nodos[j + 1][3])
+                    #d = d * (self.pic[i][j + 1] + 10)
                     self.adjList[i].append([j + 1,d])
                     self.adjList[j + 1].append([i, d])
                     flag0 = False
                 if flag1 and self.nodos[i][1] == (self.nodos[j + 1][1] or self.nodos[j + 1][0]):
                     d = self.t.distance(self.nodos[i][2], self.nodos[i][3],self.nodos[j + 1][2], self.nodos[j + 1][3])
+                    #d = d * (self.pic[j][i + 1] + 10)
                     self.adjList[i].append([j + 1,d])
                     self.adjList[j + 1].append([i, d])
                     flag1 = False
-                #m.append(0)
-            # cont += 1
-            #cont = 0
+
             flag0 = True
             flag1 = True
-        #matriz.append(m)
-        #m=[]
+
     def bestRoute(self, start, end):
         auxPath = []
         Opath, cost = self.t.dijkstra(self.adjList, start, end)
         self.shortRoute = self.t.obtenerPath(end, Opath, auxPath)
-        
-        #return self.shortRoute
         return Opath, self.shortRoute
+    
     def alternativeRoute(self,copyL, path, start, end, auxPath):
         nPath = []
-        #print(nPath)
-        
         copyPath = path
         limit = int(len(copyPath)*0.3)
         for i in range(len(copyPath) - 1):
             s = copyPath[i]
             e = copyPath[i + 1]
             if i > limit and i < len(copyPath) - limit:
-                #print(f"Eliminar de {s} a {e} ?")
                 self.t.remove(copyL, s, e) 
-                #print(f"Eliminar de {e} a {s} ?")
                 self.t.remove(copyL, e, s)
-        #print(copyG)
         OnewPath, newCost = self.t.dijkstra(copyL, start, end)
-        #print(OnewPath)
-        #return self.t.obtenerPath(end, OnewPath, auxPath)
         return OnewPath, self.t.obtenerPath(end, OnewPath, auxPath)
 
     def getNodos(self):
--- Tools.py
+++ Tools.py
@@ -8,12 +8,12 @@ class Tools:
     def distance(self, x, y, x1, y1):
         d = math.sqrt((x-x1)**2+(y-y1)**2)
         return d*100000
+
     def dijkstra(self, G, s, e):
         n = len(G)
         visited = [False]*n
         path = [-1]*n
         cost = [math.inf]*n
-
         cost[s] = 0
         pqueue = [(0, s)]
         while pqueue:
@@ -27,35 +27,28 @@ class Tools:
                             cost[v] = f
                             path[v] = u
                             hq.heappush(pqueue, (f, v))
-
         return path, cost
+
     def obtenerPath(self, i, path, zpath):
         if (i == -1):
             return list(reversed(zpath))
-        #print(f"nodo: {i} y padre {path[i]}")
         zpath.append(i) 
         return Tools.obtenerPath(self, path[i], path, zpath)
+
     def remove(self, Gaux, i, j):
         r = []
-        #cambiar a indexadeo
         for k in range(len(Gaux[i])):
-            #print(f"{Gaux[i][k][0]} es igual a {j} ?")
             if Gaux[i][k][0] == j:
                 Gaux[i].pop(k)
-                #np.delete(Gaux[i],k)
-                    #del G[i][j]
-                #print(f"Se ah quitado {Gaux[i][k]} de {Gaux[i]}")
-                #print(f"Resultado: {Gaux[i]}")
                 break
+
     def convert(adj, V):
         matrix = [[0 for j in range(V)] for i in range(V)]
-     
         for i in range(V):
             for j in adj[i]:
-                #print(adj[i][j[0]])
                 matrix[i][j[0]] = j[1]
-     
         return list(matrix)
+        
     def matrixPerlinNoiese(self, x, y):
         noise = PerlinNoise(octaves=1, seed=400)
         xpix, ypix = x, y
